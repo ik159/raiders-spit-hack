5ccdf004a6417ce66b832a1170dbe999688de042
Ishan Kumar <itsik159@gmail.com>
2022-01-29 09:31:40 +0530

    react boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+Team Members: 
+- Simran Saluja  (https://github.com/simran-saluja03)
+- Ishan Kumar  (https://github.com/ik159)
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Boiler Plate
+    </div>
+  );
+}
+
+export default App;
